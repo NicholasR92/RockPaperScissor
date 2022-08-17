075287e282fdcbf141aaa47dd9e015742f5ef043
Nick Rodriguez <nicholasrodri92@gmail.com>
2022-08-17 17:16:09 -0500

    create function for round of game

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 function randomNum(){
     return Math.floor(Math.random() * 3);
 }
-const playerSelection = prompt("what you picking?", '');
-const computerSelection = getComputerChoice();
 
 function getComputerChoice(){
     let choice = randomNum();
@@ -15,3 +13,22 @@ function getComputerChoice(){
         return "Scissors"; 
     } 
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK" || 
+       playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS" || 
+       playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() == "PAPER"){
+        return "Player Wins";
+    } else if(playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "SCISSORS" || 
+              playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() == "PAPER" || 
+              playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() == "ROCK") {
+        return "You lose";
+    } else{
+        return "Seems great minds think a like";
+    }
+}
+
+const playerSelection = prompt("what you picking?", '');
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
